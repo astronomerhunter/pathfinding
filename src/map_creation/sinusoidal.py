4d2bf9c1c66ae0ff80b02618ec5138674d5576c9
Hunter Brooks <hunterbrooks@Hunters-MBP.fios-router.home>
2017-07-28 13:31:17 -0400

    update rest of map creation algs

--- src/map_creation/sinusoidal.py
+++ src/map_creation/sinusoidal.py
@@ -5,20 +5,20 @@ import numpy as np
 # -------------------------------------------------------------------------------------- #
 def create_map(configParams):
 
-    map = np.zeros([configParams['number_of_cities'],2])
+    map = np.zeros([configParams['number_of_nodes'],2])
 
     xFunctionInput = np.linspace(0,
-                                 np.pi*configParams['number_of_x_humps'], 
-                                 configParams['number_of_cities'])
+                                 np.pi*configParams['number_of_x_groups'], 
+                                 configParams['number_of_nodes'])
     map[:,0] = np.random.choice(xFunctionInput,
-                                size=configParams['number_of_cities'],
+                                size=configParams['number_of_nodes'],
                                 p=np.sin(xFunctionInput)**2/np.sum(np.sin(xFunctionInput)**2))
 
     yFunctionInput = np.linspace(0,
-                                 np.pi*configParams['number_of_y_humps'],
-                                 configParams['number_of_cities'])
+                                 np.pi*configParams['number_of_y_groups'],
+                                 configParams['number_of_nodes'])
     map[:,1] = np.random.choice(yFunctionInput,
-                                size=configParams['number_of_cities'],
+                                size=configParams['number_of_nodes'],
                                 p=np.sin(yFunctionInput)**2/np.sum(np.sin(yFunctionInput)**2))
     
     # normalize
