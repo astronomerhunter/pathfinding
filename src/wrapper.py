2cf3ad66868497eaf67c285976af8a737cc863e5
astronomerhunter <seanhunterbrooks@gmail.com>
2017-09-11 18:34:33 -0400

    wrapper is cyclic

--- src/wrapper.py
+++ src/wrapper.py
@@ -2,16 +2,16 @@
 Words go here.
 
 Usage:
-    wrapper.py N MAPS ALGS [-g]
+    wrapper.py N MAPS ALGS [-g] [--cycles=<10>]
 
 Options:
     --version     Show version.
     -g --gif      Creates a .gif of the solution.
-
-
+    --cycles=<N>  How many times would you like to run the provided process.
 
 """
 import copy
+import numpy as np
 from docopt import docopt
 from create_map import make_map
 from execute import apply_solver
@@ -32,7 +32,7 @@ createMapArguments = {
         'fixed_number_of_groups': False,
         'random_uniform': False,
         'sinusoidal': False
-    }
+}
 executeArguments = {
     '--force': False,
     '--gif': False,
@@ -54,40 +54,45 @@ def main(cliArguments):
 
     cliArguments['MAPS'] = cliArguments['MAPS'].split(',')
     cliArguments['ALGS'] = cliArguments['ALGS'].split(',')
+
     
-    for mapName in cliArguments['MAPS']:
 
-        createMapTempArgs = copy.deepcopy(createMapArguments)
-        if mapName not in createMapTempArgs.keys():
-            print 'Invalid map name:',mapName
-            continue
-    
-        createMapTempArgs[mapName] = True
-        createMapTempArgs['N'] = int(cliArguments['N'])
-        createMapTempArgs['TECHNIQUE'] = mapName
-        createMapTempArgs['--save'] = True
-        statusCode, node_metadata, node_locations = make_map(createMapTempArgs)
-        solutions[node_metadata['map_id']] = {
-            'metadata': createMapTempArgs['TECHNIQUE']
-        }
+    for cycleIndex in np.arange(0, int(cliArguments['--cycles'])):
 
-        for algName in cliArguments['ALGS']:
+        for mapName in cliArguments['MAPS']:
 
-            executeTempArgs = copy.deepcopy(executeArguments)
-            if algName not in executeTempArgs.keys():
-                print 'Invalid alg name:',algName
+            createMapTempArgs = copy.deepcopy(createMapArguments)
+            if mapName not in createMapTempArgs.keys():
+                print 'Invalid map name:',mapName
                 continue
         
-            executeTempArgs[algName] = True
-            executeTempArgs['SOLVER'] = algName
-            executeTempArgs['MAP_ID'] = node_metadata['map_id']
-            executeTempArgs['--save'] = True
-            executeTempArgs['--force'] = True
-            if cliArguments['--gif'] is True:
-                executeTempArgs['--gif'] = True
-            statusCode, solution = apply_solver(executeTempArgs)
-            solutions[node_metadata['map_id']][solution['sol_id']] = solution
+            createMapTempArgs[mapName] = True
+            createMapTempArgs['N'] = int(cliArguments['N'])
+            createMapTempArgs['TECHNIQUE'] = mapName
+            createMapTempArgs['--save'] = True
+            statusCode, node_metadata, node_locations = make_map(createMapTempArgs)
+            solutions[node_metadata['map_id']] = {
+                'metadata': createMapTempArgs['TECHNIQUE']
+            }
 
+            for algName in cliArguments['ALGS']:
+
+                executeTempArgs = copy.deepcopy(executeArguments)
+                if algName not in executeTempArgs.keys():
+                    print 'Invalid alg name:',algName
+                    continue
+            
+                executeTempArgs[algName] = True
+                executeTempArgs['SOLVER'] = algName
+                executeTempArgs['MAP_ID'] = node_metadata['map_id']
+                executeTempArgs['--save'] = True
+                executeTempArgs['--force'] = True
+                if cliArguments['--gif'] is True:
+                    executeTempArgs['--gif'] = True
+                statusCode, solution = apply_solver(executeTempArgs)
+                solutions[node_metadata['map_id']][solution['sol_id']] = solution
+
+    # Print all the soltions after everything is finished running.
     for mapKey in solutions.keys():
         for solKey in solutions[mapKey].keys():
             if solKey != 'metadata':
@@ -96,4 +101,6 @@ def main(cliArguments):
 
 if __name__ == '__main__':
     cliArguments = docopt(__doc__, version='Wrapper 1.0')
+    if cliArguments['--cycles'] == None:
+            cliArguments['--cycles'] = 1
     main(cliArguments)
